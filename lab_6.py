2599d0df067550d981324de5af32c592bc9644a5
anas21-meet <anas21@meet.mit.edu>
2019-07-28 09:48:03 -0400

    Stopping here

---
+++ lab_6.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+  result=[]
+  for number in range(1,n):
+      if number%3==0 and number%5==0:
+        result.append('FizzBuzz')
+      elif number%3==0:
+        result.append('Fizz')
+      elif number%5==0:
+        result.append("Buzz")
+      else:
+        result.append(number)
+        
+  return(result)
---
+++ turtleshapes.py
@@ -0,0 +1,16 @@
+import turtle
+#num_pts was 5 I changed it to 9 to increase sides.
+num_pts=9
+for i in range(num_pts):
+    turtle.left(360/num_pts)
+    turtle.fd(100)
+turtle.mainloop()
+
+result=[]
+for count in range(1,10):
+    if count % 3==0:
+        result.append('Fizz')
+    else:
+        result.append(count)
+        
+
